7de088a7bdc7cd9823ea42589c6db17051ae1f17
Jason Kao <jasonkao85@gmail.com>
2018-09-27 13:29:36 -0400

    updatebar

--- src/main.js
+++ src/main.js
@@ -1,6 +1,39 @@
 let data;
 
-const initialize = () => data = data.filter(d => d.caption.length > 0);
+const initialize = () => 
+  data = data
+    .filter(d => d.caption.length > 0)
+    .map(({ start, stop, foundation, caption }) => ({
+      start,
+      stop,
+      foundation,
+      caption,
+      words: caption.split(' '),
+    }));
+
+const timedWords = ({ start, stop, words }) => {
+  // {start, stop, words}
+  const numWords = words.length;
+
+  const inc = (start - stop) / (numWords + 1);
+
+};
+
+function updateWord(captionData, i) {
+  const u = d3.select(this)
+    .selectAll('span')
+    .data(timedWords(captionData));
+
+  u.enter()
+    .append('span')
+    .style('color', 'hsla(0, 0%, 100%, .86)')
+    .html(timedWord => timedWord.text + ' ')
+    .transition()
+    .delay(timedWord => captionData.start + timedWord.delay)
+    .style('color', '#000');
+
+  u.exit().remove();
+};
 
 const update = () => {
   const u = d3.select('div#text')
@@ -9,12 +42,14 @@ const update = () => {
 
   u.enter()
     .append('span')
-    .html(d => d.caption + ' ');
-}
+    .each(updateWord);
+
+  u.exit().remove();
+};
 
 d3.csv('./data/kerry-captions.csv').then(csv => {
   data = csv;
   initialize();
 
   update();
-})
+});
