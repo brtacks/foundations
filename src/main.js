0a24a0d39b256062b4fa90a4e0087123194032c6
Jason Kao <jasonkao85@gmail.com>
2018-09-27 08:51:51 -0400

    added caption spans

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 let data;
 
+const initialize = () => data = data.filter(d => d.caption.length > 0);
+
 const update = () => {
   const u = d3.select('div#text')
-        .selectAll('')
+    .selectAll('span')
+    .data(data);
+
+  u.enter()
+    .append('span')
+    .html(d => d.caption + ' ');
 }
 
 d3.csv('./data/kerry-captions.csv').then(csv => {
   data = csv;
+  initialize();
 
   update();
 })
