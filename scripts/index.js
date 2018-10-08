f40a5e87a7e22940bd438c867dad3b005bdff235
Jason Kao <jasonkao85@gmail.com>
2018-10-07 22:57:18 -0400

    tired

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,15 @@ const wsURI = 'wss://stream.watsonplatform.net/speech-to-text/api/v1/recognize' 
   '?model=es-ES_BroadbandModel';
 const ws = new WebSocket(wsURI);
 
+onOpen = evt => {
+  const message = {
+    action: 'start',
+    'content-type': 'audio/116;rate=22050',
+    timestamps: true,
+  };
+  ws.send(JSON.stringify(message));
+};
+
 ws.onopen = onOpen;
 ws.onclose = onClose;
 ws.onmessage = onMessage;
